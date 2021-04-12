ac7d4928240809a51c6f4b3078f514ffbe63b6c1
thavetta <43245361+thavetta@users.noreply.github.com>
2021-04-12 22:48:05 +0200

    Lab04

---
+++ Laby/Lab04/Reseni/Prevodnik/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Prevodnik
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += Akce;
+        }
+
+        private void Akce(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.ExceptionObject.ToString(),"AppDomain handler");
+            MessageBox.Show("IsTerminating = " + e.IsTerminating.ToString(),"AppDomain handler");
+        }
+    }
+}
