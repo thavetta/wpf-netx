47fe03d3f4e62ea0716184ebf80b2fa1a1d17b24
thavetta <43245361+thavetta@users.noreply.github.com>
2021-04-13 22:35:53 +0200

    Lab 5 a 6

---
+++ Laby/Lab06/Reseni/OsobySL/Osoby/SeznamOsob.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osoby
+{
+    public class SeznamOsob : ObservableCollection<Osoba>
+    {
+        public SeznamOsob()
+        {
+            Add(new Osoba(){Jmeno = "Martin", Prijmeni = "Kuba", Mesto = "Brno", Plat = 35000});
+            Add(new Osoba() { Jmeno = "Jana", Prijmeni = "Mala", Mesto = "Praha", Plat = 40000 });
+            Add(new Osoba() { Jmeno = "Eva", Prijmeni = "Novotna", Mesto = "Prerov", Plat = 38000 });
+            Add(new Osoba() { Jmeno = "Karel", Prijmeni = "Kral", Mesto = "Ostrava", Plat = 28000 });
+            Add(new Osoba() { Jmeno = "Jiri", Prijmeni = "Kovar", Mesto = "Liberec", Plat = 52000 });
+
+        }
+    }
+}
---
+++ Laby/Lab06/Start/OsobySL/Osoby/Osoba.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osoby
+{
+    public class Osoba : INotifyPropertyChanged
+    {
+        private string jmeno;
+
+        public string Jmeno
+        {
+            get => jmeno;
+            set
+            {
+                if (jmeno == value) return;
+                jmeno = value;
+                OnPropertyChanged(nameof(Jmeno));
+                OnPropertyChanged(nameof(KompletJmeno));
+            }
+        }
+
+        private string prijmeni;
+
+        public string Prijmeni
+        {
+            get => prijmeni;
+            set
+            {
+                if (prijmeni == value) return;
+                prijmeni = value;
+                OnPropertyChanged(nameof(Prijmeni));
+                OnPropertyChanged(nameof(KompletJmeno));
+            }
+        }
+
+        public string KompletJmeno => Jmeno + " " + Prijmeni;
+
+        private string mesto;
+
+        public string Mesto
+        {
+            get => mesto;
+            set
+            {
+                if (mesto == value) return;
+                mesto = value;
+                OnPropertyChanged(nameof(Mesto));
+            }
+        }
+
+        private int plat;
+
+        public int Plat
+        {
+            get => plat;
+            set
+            {
+                if (plat == value) return;
+                plat = value;
+                OnPropertyChanged(nameof(Plat));
+            }
+        }
+
+
+
+        //Implementace INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
