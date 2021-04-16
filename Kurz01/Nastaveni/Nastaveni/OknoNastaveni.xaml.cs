15e39e1a84bad7b1c583d68b360a209f029121c0
thavetta <tomas@havetta.cz>
2021-04-16 16:52:12 +0200

    Kurz01

---
+++ Kurz01/Nastaveni/Nastaveni/OknoNastaveni.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Nastaveni
+{
+    /// <summary>
+    /// Interaction logic for OknoNastaveni.xaml
+    /// </summary>
+    public partial class OknoNastaveni : Window
+    {
+        public OknoNastaveni()
+        {
+            InitializeComponent();
+        }
+
+        private void OKAkce(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+            this.Close();
+        }
+
+        private void CancelAkce(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Reload();
+            this.Close();
+        }
+    }
+}
---
+++ Kurz01/PrikladPocitadlo/PrikladPocitadloTests2/PocitadloTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrikladPocitadlo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrikladPocitadlo.Tests
+{
+    [TestClass()]
+    public class PocitadloTests
+    {
+        [TestMethod()]
+        public void SectiTest()
+        {
+            int a = 10;
+            int b = 20;
+            int ocekavam = 30;
+            Pocitadlo pocitadlo = new Pocitadlo();
+            int vysledek = pocitadlo.Secti(a, b);
+            Assert.AreEqual(ocekavam, vysledek);
+        }
+
+        [TestMethod()]
+        public void OdectiTest()
+        {
+            int a = 10;
+            int b = 20;
+            int ocekavam = -11;
+            Pocitadlo pocitadlo = new Pocitadlo();
+            int vysledek = pocitadlo.Odecti(a, b);
+            Assert.AreEqual(ocekavam, vysledek);
+        }
+    }
+}
