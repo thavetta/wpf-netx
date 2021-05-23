794fa6ab895e50f46a59da020ef089a8573077c9
thavetta <43245361+thavetta@users.noreply.github.com>
2021-05-23 14:41:33 +0200

    Lab 8

---
+++ Laby/Lab08/Start/Prevodnik/VypocetPrevodu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prevodnik
+{
+    public class VypocetPrevodu : INotifyPropertyChanged
+    {
+        private double vstupniHodnota;
+        public double VstupniHodnota 
+        { 
+            get => vstupniHodnota;
+            set
+            {
+                if (value == vstupniHodnota)
+                    return;
+                vstupniHodnota = value;
+                Vypocet();
+            }
+
+        }
+        
+        private double vystupniHodnota;
+
+        public double VystupniHodnota
+        {
+            get => vystupniHodnota;
+            set
+            {
+                if (value == vystupniHodnota)
+                    return;
+                vystupniHodnota = value;
+                OnPropertyChanged(nameof(VystupniHodnota));
+            }
+        }
+
+        public Func<double,double> Prevod { get; set; }
+
+        public void Vypocet()
+        {
+            VystupniHodnota = Prevod?.Invoke(VstupniHodnota) ?? 0;
+            
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
